eb069b8390a82d7d21dfc985d115dd10a86e526a
Blqsterz <lavrfell@mail.ru>
2026-03-05 17:18:39 +0300

    fifth commit

---
+++ 3_5.py
@@ -0,0 +1,9 @@
+first = 5 + 2 * 3 - 4 / 2
+second = (3+5) * (2+4) / 2
+third = -3 + 6 /2 * 4
+fourth= 5 + 4 * 5 ** 2 + 7
+
+print(f"1) {first}")
+print(f"2) {second}")
+print(f"3) {third}")
+print(f"4) {fourth}")
